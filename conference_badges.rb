0bb1a9d2a4fc12de192b7d0677f6dcc5989d1438
Jacob Menashi <jbmenashi@gmail.com>
2018-09-27 15:38:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_of_names)
+  array_of_badges = []
   array_of_names.each do |name|
-    badge_maker(name)
+    array_of_badges << badge_maker(name)
   end
 end
 
