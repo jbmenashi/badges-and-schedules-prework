eb698168eab931e2428eddf1a2119ed97c445128
Jacob Menashi <jbmenashi@gmail.com>
2018-09-27 15:49:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(array_of_names)
 end
 
 def assign_rooms(list_of_speakers)
-  assignment_list = ""
+  assignment_list = []
   list_of_speakers.each do |speaker|
     assignment_list << "Hello #{speaker}! You'll be assigned to room #{list_of_speakers.index(speaker)}!"
   end
